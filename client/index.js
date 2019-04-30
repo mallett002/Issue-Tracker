85c30ef97b97c8e70b132c77289f45be1e5cc790
William Mallett <MallettWilliam@JohnDeere.com>
2019-04-30 06:12:31 -0500

    adding client files

---
+++ client/index.js
@@ -0,0 +1,36 @@
+/*
+$(function() {
+    $('#testForm').submit(function(e) {
+        $.ajax({
+            url: '/api/issues/apitest',
+            type: 'post',
+            data: $('#testForm').serialize(),
+            success: function(data) {
+                $('#jsonResult').text(JSON.stringify(data));
+            }
+        });
+        e.preventDefault();
+    });
+    $('#testForm2').submit(function(e) {
+        $.ajax({
+            url: '/api/issues/apitest',
+            type: 'put',
+            data: $('#testForm2').serialize(),
+            success: function(data) {
+                $('#jsonResult').text(JSON.stringify(data));
+            }
+        });
+        e.preventDefault();
+    });
+    $('#testForm3').submit(function(e) {
+        $.ajax({
+            url: '/api/issues/apitest',
+            type: 'delete',
+            data: $('#testForm3').serialize(),
+            success: function(data) {
+                $('#jsonResult').text(JSON.stringify(data));
+            }
+        });
+        e.preventDefault();
+    });
+});*/
