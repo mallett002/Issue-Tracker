cd7f7965f801293bffad90490fb0e5ff6fb7f558
William Mallett <MallettWilliam@JohnDeere.com>
2019-06-12 06:14:25 -0500

    finishing update issue in db

--- controllers/createIssue.js
+++ controllers/createIssue.js
@@ -9,12 +9,13 @@ module.exports = (req, res) => {
     const date = moment();
     let created_on = date.format("dddd, MMMM Do YYYY, h:mm:ss a");
     let updated_on = "";
+    const closed_issue = "false";
 
     // Create query with form data
     const queryString = `INSERT INTO issues
-      (issue_title, issue_text, created_by, assigned_to, status_text, created_on, updated_on) 
-      VALUES (?, ?, ?, ?, ?, ?, ?)`;
-    const values = [title, text, createdBy, assignedTo, statusText, created_on, updated_on];
+      (issue_title, issue_text, created_by, assigned_to, status_text, created_on, updated_on, closed_issue) 
+      VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+    const values = [title, text, createdBy, assignedTo, statusText, created_on, updated_on, closed_issue];
 
     // Send data to db
     db.query(queryString, values, (err) => {
--- controllers/updateIssue.js
+++ controllers/updateIssue.js
@@ -2,29 +2,74 @@ const moment = require('moment');
 
 module.exports = (req, res) => {
     const id = req.body.id;
-    const title = req.body.title;
-    const text = req.body.text;
-    const createdBy = req.body.createdBy;
-    const assignedTo = req.body.assignedTo;
-    const statusText = req.body.statusText;
-    const closeIssue = req.body.closeIssue;
+    const issue_title = req.body.title;
+    const issue_text = req.body.text;
+    const created_by = req.body.createdBy;
+    const assigned_to = req.body.assignedTo;
+    const status_text = req.body.statusText;
     const date = moment();
     const updated_on = date.format("dddd, MMMM Do YYYY, h:mm:ss a");
+    const closed_issue = req.body.closeIssue ? "true" : "";
 
-    // use id to find one and update
-    console.log(`Searching db for id: ${id}`);
-
-    const payload = {
-        title,
-        text,
-        createdBy,
-        assignedTo,
-        statusText,
-        closeIssue,
-        updated_on
+    const valuesByDBName = {
+        issue_title,
+        issue_text,
+        created_by,
+        assigned_to,
+        status_text,
+        updated_on,
+        closed_issue,
+        id
     };
 
-    console.log({payload});
+    const updatedValues = addValuesToUpdate(valuesByDBName);
+    const updateIssueQuery = getUpdateQueryString(valuesByDBName);
+    const getIssueQuery =  `SELECT issue_title FROM issues WHERE id = ?`;
 
-    res.status(201).send(`Updated issue with title ${title}`);
+    db.query(getIssueQuery, [id], (err, result) => {
+        if (err || !result.length) {
+            return res.status(500).send(err);
+        }
+    });
+
+    db.query(updateIssueQuery, updatedValues, (err) => {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        res.redirect('/');
+    });
 };
+
+function addValuesToUpdate(values) {
+    let newValues = [];
+
+    Object.keys(values).forEach(key => {
+       if (values[key]) {
+           newValues.push(values[key]);
+       }
+    });
+    return newValues;
+}
+
+// UPDATE issues SETTony = ?, Wednesday, June 12th 2019, 5:04:25 am = ?, false = ?, 55 = ?55 = ?,  WHERE id = ?
+function getUpdateQueryString(values) {
+    let query = "UPDATE issues SET";
+    let valuesToUpdate = [];
+
+    Object.keys(values).forEach(key => {
+        if (values[key] && key !== "id") {
+            valuesToUpdate.push(key);
+        }
+    });
+
+    valuesToUpdate.forEach((val, i, arr) => {
+       if (i === arr.length - 1) {
+           query += " " + val + " = ?";
+       } else {
+           query += " " + val + " = ?,";
+       }
+    });
+
+    query += " WHERE id = ?";
+    return query;
+}
