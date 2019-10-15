ace6fff3b1ecb061aaea1c98904198c2e318b0c5
William Mallett <mallett002@gmail.com>
2019-10-15 05:58:28 -0500

    trying to add docker compose

--- Dockerfile
+++ Dockerfile
@@ -11,9 +11,11 @@ COPY package*.json ./
 # RUN npm install
 # If you are building your code for production
 RUN npm ci --only=production
+RUN npm run build
 
 # Bundle app source
 COPY . .
 
-EXPOSE 8080
+EXPOSE 3000
+
 CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -53,10 +53,6 @@ app.use((req, res) => {
         .send('Not found');
 });
 
-app.listen(process.env.port || 3000, () => {
-    process.env.port
-        ? console.log("Server is running on port " + process.env.port)
-        : console.log("Server is running on port 3000");
-});
+app.listen(3000, () => console.log("Server is running on port 3000"));
 
 module.exports = app;
